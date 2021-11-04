fc5a2560f6edb4a01a7466cdd23a46d50d2a66c6
Anju Bhole <anjusbhole@gmail.com>
2021-11-04 09:59:48 -0600

    Adding validation & code cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,25 +14,32 @@ export class AppComponent {
   userName: string = '';
   userSearchResponse: any;
   dataService: DataService;
-  userSearchResponseItems: any;
+  userSearchResponseItems: any = [];
   selectedRow: any;
+  isOnLoad = true;
   /*Search results grid*/
   columnDefs = [
+    {headerName: 'User Name', field: 'display_name'},
     {headerName: 'Account Id', field: 'account_id'},
     {headerName: 'User Id', field: 'user_id'},
-    {headerName: 'Display Name', field: 'display_name'},
   ];
-  rowData = [];
 
   constructor(public httpClient: HttpClient, private modalService: NgbModal){
     this.dataService = new DataService(httpClient);
   }
   async onSearch(){
     console.log("Search Request received with Username : " + this.userName);
+    /*RegExp to validate the special characters and return request*/
+    const regexp = new RegExp("^[A-Za-z0-9 ]*$");
+    if (!regexp.test(this.userName)){
+      this.userSearchResponseItems = [];
+      this.isOnLoad = false;
+      return;
+    }
     this.userSearchResponse = await this.dataService.getSearchUserData(this.userName);
     this.userSearchResponseItems = this.userSearchResponse.items;
-    this.rowData = this.userSearchResponseItems;
-    console.log('Response from StackExchange : \n' + this.userSearchResponse);
+    console.log('Response received from StackExchange API');
+    this.isOnLoad = false;
   }
   open(content:any, event: any) {
     this.selectedRow = event.data;
